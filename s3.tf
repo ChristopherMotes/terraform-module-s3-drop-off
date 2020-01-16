ca23e83447ed996aa50bb9a22bb1abe2acc4aa24
Christopher <motescj@aim.com>
2020-01-16 22:34:04 +0000

    add logging

--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "dropOffBucket" {
     bucket = "${var.bucketName}.imperial.galactic.gov"
     acl    = "private"
+    logging {
+        target_bucket = var.loggingBucketName
+        target_prefix = "${var.bucketName}/"
+    }
 }
