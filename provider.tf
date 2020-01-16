065dcff0d96fe7fe627bdbe20da820fad685e154
Christopher <motescj@aim.com>
2020-01-16 17:17:14 +0000

    add backend info

--- provider.tf
+++ provider.tf
@@ -1,3 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state.imperial.galactic.gov"
+    region = "us-east-2"
+  }
+}
+
 provider "aws" {
   version = "~> 2.0"
   region  = "us-east-2"
